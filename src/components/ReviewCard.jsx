0ddd032d9f7c1e6ae2422b9282fc914d752abb84
alexrongg <alexrong19@gmail.com>
2022-07-28 14:54:10 +0100

    added voting functionality on review pages

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom"
+const axios = require("axios")
 
 export default function ReviewCard() {
 
     const [reviews, setReviews] = useState([]);
     const {category}= useParams()
+    const [vote, setVote] = useState(false)
 
     
     useEffect(() => {
@@ -17,16 +19,29 @@ export default function ReviewCard() {
             .then((res) => res.json())
             .then((items) => setReviews(items)) 
         }
-    }, [category]); 
+    }, [category, vote]); 
+
+    const addVoteHandler = (review_id) => {
+        setVote(false)
+        axios
+        .patch(`https://alex-games.herokuapp.com/api/reviews/${review_id}`, {"inc_votes" : 1})
+        .then(() => setVote(true))
+    }
+    const removeVoteHandler = (review_id) => {
+        setVote(false)
+        axios
+        .patch(`https://alex-games.herokuapp.com/api/reviews/${review_id}`, {"inc_votes" : -1})
+        .then(() => setVote(true))
+    }
     
 
  return (reviews.map((review) => {
     return (
         <div className="review-card" key={review.review_id}>
             <img src={review.review_img_url} alt={`${review.owner}'s game review`}></img>
-            <button>⬆️</button>
+            <button onClick={()=> {addVoteHandler(review.review_id)}}>⬆️</button>
             <h3>{review.votes}</h3>
-            <button>⬇️</button>
+            <button onClick={()=> {removeVoteHandler(review.review_id)}}>⬇️</button>
             <h2>{review.title} by {review.designer}</h2>
             <p>{review.review_body.split('', 300)}<Link to={`../review/${review.review_id}`}> read more...</Link></p>
             <p>Category: {review.category} <br></br>
